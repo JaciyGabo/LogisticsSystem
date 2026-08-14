aa480dd3ee87e90226926d00e442cf6def2f9b6e
JaciyGabo <jacielquiroga70@gmail.com>
2026-08-13 23:20:35 -0600

    fix(api): resolve dependency injection namespaces and configure controller routing

--- Logistics.API/Program.cs
+++ Logistics.API/Program.cs
@@ -1,6 +1,8 @@
 
 using Logistics.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Logistics.Domain.Repositories;
+using Logistics.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,24 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
 
